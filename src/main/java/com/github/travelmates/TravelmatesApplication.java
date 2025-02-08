220213982146d234362a8c1829f4ddb733943578
Mickael <mickaeldesclaux@gmail.com>
2025-02-08 09:53:16 +0100

    project init

---
+++ src/main/java/com/github/travelmates/TravelmatesApplication.java
@@ -0,0 +1,13 @@
+package com.github.travelmates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelmatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelmatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/travelmates/TravelmatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.travelmates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelmatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
